d359dcedff0d55e2e3ef8b205217db1665289390
Igor Matos <igor.matos@liferay.com>
2017-11-06 14:54:08 -0300

    atualizar gradle

--- app/src/main/java/com/liferay/raysbanking/LoginActivity.java
+++ app/src/main/java/com/liferay/raysbanking/LoginActivity.java
@@ -39,7 +39,7 @@ public class LoginActivity extends AppCompatActivity implements LoginListener {
 		binding.signUp.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Intent intent = new Intent(view.getContext(), AccountActivity.class);
+				Intent intent = new Intent(view.getContext(), SignUpActivity.class);
 				startActivity(intent);
 			}
 		});
